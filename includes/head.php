c95000154d29e632de0e3312c1d0e0147bbe29e4
vivien okros <figfog11@gmail.com>
2024-08-05 16:19:42 +0200

    My changes

---
+++ includes/head.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <script src="/js/bootstrap.bundle.min.js"></script>
+    <title>Learning PHP</title>
+</head>
